5ec16485595cb9e7ee52ff62a32886940f4e5e19
ujeans <dbwlsxkal95@gmail.com>
2024-10-04 00:08:05 +0900

    kakao 기본 마커 구현하기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,21 +2,43 @@
 
 import Script from "next/script";
 
+import * as stores from "@/data/store_data.json";
+
 declare global {
   interface Window {
     kakao: any;
   }
 }
 
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03888379;
+
 export default function Map() {
   const loadKakaoMap = () => {
     window.kakao.maps.load(() => {
-      const mapContainer = document.getElementById("map");
+      const mapContainer = document.getElementById("map"); // 지도를 표시할 div
       const mapOption = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG), // 지도의 중심 좌표
+        level: 3, // 지도의 확대 레벨
       };
-      new window.kakao.maps.Map(mapContainer, mapOption);
+      const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성
+
+      // 식당 데이터 마커 띄우기
+      stores?.["DATA"]?.map(store => {
+        // 마커가 표시될 위치입니다
+        const markerPosition = new window.kakao.maps.LatLng(
+          store?.y_dnts,
+          store?.x_cnts
+        );
+
+        // 마커를 생성합니다
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+      });
     });
   };
 
