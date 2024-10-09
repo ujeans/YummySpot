77b3180a0a98d80fb8454af607dea32d6f8882d5
ujeans <dbwlsxkal95@gmail.com>
2024-10-10 02:12:37 +0900

    useIntersectionObserver hook 만들기

---
+++ src/hook/useIntersectionObserver.ts
@@ -0,0 +1,34 @@
+import { RefObject, useEffect, useState } from "react";
+
+function useIntersectionObserver(
+  elementRef: RefObject<Element>,
+  { threshold = 0.1, root = null, rootMargin = "0%" }
+) {
+  const [entry, setEntry] = useState<IntersectionObserverEntry>();
+
+  // 변경이 있는 경우 callback을 등록해서
+  const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry);
+  };
+
+  useEffect(() => {
+    const node = elementRef?.current;
+    const hasIOSupport = !!window.IntersectionObserver;
+
+    if (!node || !hasIOSupport) return;
+
+    const observerParams = { threshold, root, rootMargin };
+
+    const observer = new IntersectionObserver(updateEntry, observerParams);
+
+    observer.observe(node);
+
+    return () => observer.disconnect;
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [elementRef?.current, root, rootMargin, JSON.stringify(threshold)]);
+
+  return entry;
+}
+
+export default useIntersectionObserver;
