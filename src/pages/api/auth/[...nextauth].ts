234f9b14ffabe6dfbd073e942a6cf16e8006a865
ujeans <dbwlsxkal95@gmail.com>
2024-10-10 14:32:40 +0900

    prisma adpater 세팅

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+
   // Configure one or more authentication providers
   providers: [
     // ...add more providers here
