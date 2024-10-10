bd7a4ae7585512d4353564c3dd57021ba470229e
ujeans <dbwlsxkal95@gmail.com>
2024-10-10 15:05:21 +0900

    Google Login 세팅

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import GoogleProvider from "next-auth/providers/google";
+
 const prisma = new PrismaClient();
 
 export const authOptions = {
@@ -10,6 +12,10 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
 };
 
